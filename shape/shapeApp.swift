91dfc067804b56ce4badbaf802faf947771837f7
Marcelo <marcelo.neri@iest.edu.mx>
2025-08-29 18:40:51 -0600

    Initial Commit

---
+++ shape/shapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  shapeApp.swift
+//  shape
+//
+//  Created by win603 on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct shapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
